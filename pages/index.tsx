89aafe62d2637fbc7c93b52b730c5ab1ea918e14
Honore Hounwanou <mercuryseries@gmail.com>
2022-11-20 17:46:21 -0500

    Remove duplication

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,12 +23,11 @@ const footerNavigation = {
   main: [
     { name: 'The Story', href: '#' },
     { name: 'Documentation', href: '#' },
-    { name: 'Datamol Fingerprints', href: '#' },
-    { name: 'GitHub', href: '#' }
+    { name: 'Datamol Fingerprints', href: '#' }
   ],
   social: [
     {
-      name: 'Twitter',
+      name: 'Follow us on Twitter',
       href: 'https://twitter.com/datamol_io',
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
@@ -37,7 +36,17 @@ const footerNavigation = {
       )
     },
     {
-      name: 'GitHub',
+      name: 'Discuss on GitHub',
+      href: 'https://github.com/datamol-org/datamol/discussions',
+      icon: (props) => (
+        <svg fill="currentColor" viewBox="0 0 20 20" {...props}>
+          <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z"></path>
+          <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z"></path>
+        </svg>
+      )
+    },
+    {
+      name: 'View Source Code',
       href: 'https://github.com/datamol-org/datamol',
       icon: (props) => (
         <svg fill="currentColor" viewBox="0 0 24 24" {...props}>
@@ -172,7 +181,7 @@ export default function Home() {
                   <a
                     key={item.name}
                     href={item.href}
-                    className="font-semibold text-gray-900 hover:text-gray-900"
+                    className="font-semibold text-gray-700 transition duration-200 ease-in-out hover:text-gray-900"
                   >
                     {item.name}
                   </a>
@@ -180,10 +189,48 @@ export default function Home() {
               </div>
               <div className="hidden lg:flex lg:min-w-0 lg:flex-1 lg:justify-end">
                 <a
-                  href="#"
-                  className="inline-block rounded-lg px-3 py-1.5 text-sm font-semibold leading-6 text-gray-900 shadow-sm ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+                  href="https://twitter.com/datamol_io"
+                  className="inline-block px-3 py-1.5 text-sm font-semibold leading-6 text-gray-700 transition duration-200 ease-in-out hover:text-gray-900"
                 >
-                  Log in
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    className="h-6 w-6"
+                    aria-hidden="true"
+                  >
+                    <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                  </svg>
+                </a>
+                <a
+                  href="https://github.com/datamol-org/datamol/discussions"
+                  className="inline-block px-3 py-1.5 text-sm font-semibold leading-6 text-gray-700 transition duration-200 ease-in-out hover:text-gray-900"
+                >
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    className="h-6 w-6"
+                    aria-hidden="true"
+                  >
+                    <path d="M2 5a2 2 0 012-2h7a2 2 0 012 2v4a2 2 0 01-2 2H9l-3 3v-3H4a2 2 0 01-2-2V5z"></path>
+                    <path d="M15 7v2a4 4 0 01-4 4H9.828l-1.766 1.767c.28.149.599.233.938.233h2l3 3v-3h2a2 2 0 002-2V9a2 2 0 00-2-2h-1z"></path>
+                  </svg>
+                </a>
+                <a
+                  href="https://github.com/datamol-org/datamol"
+                  className="inline-block px-3 py-1.5 text-sm font-semibold leading-6 text-gray-700 transition duration-200 ease-in-out hover:text-gray-900"
+                >
+                  <svg
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                    className="h-6 w-6"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
                 </a>
               </div>
             </nav>
@@ -228,12 +275,15 @@ export default function Home() {
                       ))}
                     </div>
                     <div className="py-6 text-center">
-                      <a
-                        href="#"
-                        className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
-                      >
-                        Log in
-                      </a>
+                      {footerNavigation.social.map((item) => (
+                        <a
+                          href={item.href}
+                          target="_blank"
+                          className="-mx-3 block rounded-lg py-2.5 px-3 text-base font-semibold leading-6 text-gray-900 hover:bg-gray-400/10"
+                        >
+                          {item.name}
+                        </a>
+                      ))}
                     </div>
                   </div>
                 </div>
@@ -438,14 +488,11 @@ export default function Home() {
                 <a
                   key={item.name}
                   href={item.href}
-                  className="text-gray-400 hover:text-gray-500"
+                  className="text-gray-400 transition duration-200 ease-in-out hover:text-gray-500"
                   target="_blank"
                 >
                   <span className="sr-only">{item.name}</span>
-                  <item.icon
-                    className={item.name === 'Discuss' ? 'h-7 w-7' : 'h-6 w-6'}
-                    aria-hidden="true"
-                  />
+                  <item.icon className="h-6 w-6" aria-hidden="true" />
                 </a>
               ))}
             </div>
@@ -454,7 +501,7 @@ export default function Home() {
               <a
                 href="https://valencediscovery.com"
                 target="_blank"
-                className="border-b border-dotted border-gray-200 transition duration-200 hover:border-gray-300"
+                className="border-b border-dotted border-gray-200 transition duration-200 ease-in-out hover:border-gray-300"
               >
                 Valence Discovery
               </a>
